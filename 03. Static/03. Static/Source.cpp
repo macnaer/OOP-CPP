31747ae5ed3ade589ea3c253bb2b9df581e947bd
k2 <trofimchuk.an@gmail.com>
2020-12-06 00:17:53 +0200

    static

--- 03. Static/03. Static/Source.cpp
+++ 03. Static/03. Static/Source.cpp
@@ -20,6 +20,10 @@ public:
 		this->price = price;
 	}
 
+	static void ChangePrice(CarFactory &car, int price) {
+		car.price = price;
+	}
+
 	void ShowCarInfo() {
 		cout << "Vendor: " << this->vendor << "\nModel: " << this->model << "\nEngine: " << this->engine
 			<< "\nPrice: " << this->price << "\nCar Number: " << this->CarsCount << endl;
@@ -29,10 +33,23 @@ public:
 int CarFactory::CarsCount = 0;
 int main() {
 
+	cout << "Car 1=========== " << endl;
 	CarFactory car1("Audi", "A5", "Deisel", 1200);
 	car1.ShowCarInfo();
 
+	cout << "Car 2=========== " << endl;
 	CarFactory car2("Audi", "A4", "Deisel", 1000);
+	car2.ShowCarInfo();
+
+
+	cout << "Car 1=========== " << endl;
+	cout << "===========================" << endl;
+	//car1.ChangePrice(car1, 20000);
+	CarFactory::ChangePrice(car1, 23000);
+	car1.ShowCarInfo();
+
+
+	cout << "Car 2=========== " << endl;
 	car2.ShowCarInfo();
 
 	system("pause");
