4d46e6ccac1a038fca9f8740720bfb16a8b8481d
k2 <trofimchuk.an@gmail.com>
2021-01-24 12:24:07 +0200

    STL

--- 10. STL/10. List/Source.cpp
+++ 10. STL/10. List/Source.cpp
@@ -1,68 +1,115 @@
 #include <iostream>
 #include <list>
-#include <deque>
 #include <vector>
-
+#include <deque>
 
 using namespace std;
 
-
 template <typename T>
-void Print(const list<T> &lst) {
+void PrintList(const list<T> &lst) {
 	for (auto i = lst.begin(); i != lst.end(); ++i) {
 		cout << *i << endl;
+	}
+}
+
+template <typename T>
+void PrintDeque(const deque<T> &dq) {
+	for (auto i = dq.begin(); i != dq.end(); ++i) {
+		cout << *i << endl;
+	}
+}
+
+template <typename T>
+void PrintVector(const vector<T> &vect) {
+	/*for (auto i = vect.begin(); i != vect.end(); ++i) {
+		cout << *i << endl;
+	}*/
+	for (int i = 0; i < vect.size(); i++) {
+		cout << vect[i] << endl;
 	}
 }
 
 int main() {
 
 #pragma region List
-	/*list<int> lst;
+	//list<int> lst = {1,7,9};
+	//list<int>::iterator it = lst.begin();
+	//
+	//PrintList(lst);
+	//cout << "===========Print default list=========" << endl;
+	//lst.push_back(8);
+	//PrintList(lst);
+	//cout << "===========Print push back=========" << endl;
+	//lst.push_front(10);
+	//PrintList(lst);
+	//cout << "===========Print push front=========" << endl;
+	//cout << "List size = " << lst.size() << endl;
+	//lst.sort();
+	//PrintList(lst);
+	//cout << "===========Print sort list=========" << endl;
+	////lst.reverse();
+	////PrintList(lst);
+	////cout << "===========Print reverse list=========" << endl;
+	//lst.pop_back();
+	//PrintList(lst);
+	//cout << "===========Print remove last=========" << endl;
+	//advance(it, 2);
+	//lst.insert(it,100);
+	//lst.insert(it, 100);
+	//PrintList(lst);
+	//cout << "===========Print insert by iterator=========" << endl;
+	//lst.remove(100);
+	//PrintList(lst);
+	//cout << "===========Print delete by value=========" << endl;
+	//cout << "Iterator position => " << *it << endl;
+	///*advance(it, 1);*/
+	//--it;
+	//--it;
+	//cout << "Iterator position => " << *it << endl;
+	//lst.erase(it);
+	//PrintList(lst);
+	//cout << "===========Print erase by iterator=========" << endl;
 
-	lst.push_back(10);
-	lst.push_front(5);
-	lst.push_front(25);
-	lst.push_front(35);
+#pragma endregion
+#pragma region Vector
+	//vector<int> vect = { 5,1,4 };
+	//PrintVector(vect);
+	//cout << "=============Print defaut vector==============" << endl;
+	//vect.push_back(30);
+	//PrintVector(vect);
+	//cout << "=============Print push_back==============" << endl;
+	////cout << "vector => " << vect.at(2) << endl;
+	//PrintVector(vect);
+	//cout << "=============Print replace by hardcode==============" << endl;
+	//vector<int>::iterator it = vect.begin();
+	//vect.insert(it + 1, 4);
+	//PrintVector(vect);
+	//cout << "=============Print incert==============" << endl;
 
-	lst.sort();
-	Print(lst);
-	cout << lst.size() << endl;
-	cout << "======================" << endl;
-	lst.pop_front();
 
-	Print(lst);
-	cout << lst.size() << endl;
-	lst.reverse();
-	cout << "==========Reverse==========" << endl;
-	Print(lst);*/
-	#pragma endregion	
+
+#pragma endregion
 
 #pragma region Deque
 
-	//deque<int> arrDeque = { 5,3,7,9,8 };
-	//arrDeque.push_back(100);
-	//arrDeque.push_front(200);
+	deque<int> dq = { 7 ,3,4,7,8,9 };
 
-	//for (auto i = arrDeque.begin(); i != arrDeque.end(); ++i) {
-	//	cout << *i << endl;
-	//}
-	#pragma endregion
+	PrintDeque(dq);
+	cout << "==============Print default duque=========" << endl;
+	dq.push_back(10);
+	dq.push_front(20);
+	PrintDeque(dq);
+	cout << "==============Print push_back push_front==============" << endl;
+	dq.insert(dq.begin() + 2, 10);
+	PrintDeque(dq);
+	cout << "==============Print incert==============" << endl;
+	dq.erase(dq.begin() + 1);
+	PrintDeque(dq);
+	cout << "==============Delete incert==============" << endl;
 
-#pragma region Vector
-	vector<int> arrVector;
-	arrVector.push_back(4);
-	arrVector.push_back(5);
-	arrVector.push_back(1);
-	arrVector.push_back(9);
 
-	for (int i = 0; i < arrVector.size(); i++) {
-		cout << "arrVector => " << arrVector[i] << endl;
-	}
-
-	cout << "arrVector.at " << arrVector.at(2) << endl;
-	cout << "arrVector size " << arrVector.size() << endl;
-	cout << "arrVector capasity " << arrVector.capacity() << endl;
 #pragma endregion
+
 
 
 	system("pause");
